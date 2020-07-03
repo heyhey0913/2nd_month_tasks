522c67131d3d0c265fbe2a972a37742fe8e32724
vagrant <vagrant@localhost.localdomain>
2020-07-03 01:40:51 +0000

    [finish]task2

---
+++ task2/bk2-before_debug/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+  sessions: 'users/sessions',
+  registrations: 'users/registrations'
+}
+  root 'home#top'
+  get 'home/about'
+
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+end
